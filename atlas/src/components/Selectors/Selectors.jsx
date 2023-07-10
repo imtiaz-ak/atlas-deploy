379c4ec43713f69ed4335fffc8e916c6260c50db
mirza-adnan <mirza.adnan2205@gmail.com>
2023-07-10 23:02:55 +0600

    finished styling the selectors

--- atlas/src/components/Selectors/Selectors.jsx
+++ atlas/src/components/Selectors/Selectors.jsx
@@ -1,25 +1,50 @@
-import React from 'react'
+import React from "react";
 import arrow from "../../assets/arrow.svg";
+import { useState } from "react";
 
 function Selectors() {
-  return (
-    <div className="selectors-container">
-        <div className="dataset-selector">
-            <button className="btn dataset-selector-btn">
-                <img src={arrow} alt="arrow" />
-                <div className="dataset-selector-text selector-text">
-                    <p className="weather-type">HOT WEATHER</p>
-                    <p className="weather-variable">Number of Cooling degree days</p>
+    const [isMore, setIsMore] = useState(false);
+
+    const handleMoreClick = () => {
+        setIsMore(true);
+    };
+
+    const handleLessClick = () => {
+        setIsMore(false);
+    };
+
+    return (
+        <div className="selectors-container">
+            <div className="dataset-selector selector">
+                <button className="btn dataset-selector-btn">
+                    <img src={arrow} alt="arrow" />
+                    <div className="dataset-selector-text selector-text">
+                        <p className="weather-type">HOT WEATHER</p>
+                        <p className="weather-variable">
+                            Number of Cooling degree days
+                        </p>
+                    </div>
+                </button>
+            </div>
+            <div className="intensity-selector selector">
+                <div className="intensity-text selector-text">
+                    <p className="emission-text">GHG EMISSION</p>
+                    <p className="climate-change-text">Climate Change</p>
                 </div>
-            </button>
-        </div>
-        <div className="intensity-selector">
-            <div className="intensity-text selector-text">
-                <p className="emission">GHG EMISSION</p>
-                <p className="climate-change-text">Climate Change</p>
+                <div className="intensity-buttons">
+                    <button
+                        className={`btn intensity-btn ${!isMore && "active"}`}
+                        onClick={handleLessClick}>
+                        Less
+                    </button>
+                    <button
+                        className={`btn intensity-btn ${isMore && "active"}`}
+                        onClick={handleMoreClick}>
+                        More
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-  )
-  }
-export default Selectors
+    );
+}
+export default Selectors;
