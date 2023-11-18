16ac2a40c2511322cf2f6283aed0bf44f75945ab
Imtiaz Ahmed Khan <ak.imtiaz147@gmail.com>
2023-11-18 22:19:12 +0600

    map not changing - bug fix

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -419,7 +419,7 @@ export default function Map() {
                 svg.attr("transform", e.transform);
             })
         );
-    }, []); //[datasetName, datasetEmission, datasetTimeline]);
+    }, [datasetName, datasetEmission, datasetTimeline]); //[datasetName, datasetEmission, datasetTimeline]);
 
     return (
         <>
